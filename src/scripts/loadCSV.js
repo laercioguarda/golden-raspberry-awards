42853f79c4069105f897ca05e481881e927429c9
laercioguarda <laercioguarda@gmail.com>
2025-02-17 09:28:41 -0300

    Adicionado validações para o CSV

--- src/scripts/loadCSV.js
+++ src/scripts/loadCSV.js
@@ -19,10 +19,31 @@ async function loadCSV() {
 
     return new Promise((resolve, reject) => {
       const movies = [];
+      let headersValidated = false;
+      let hasData = false;
+      const requiredFields = ["year", "title", "studios", "producers", "winner"];
 
       fs.createReadStream(csvFilePath)
         .pipe(csv({ separator: ";" }))
+        .on("headers", (headers) => {
+          const missingFields = requiredFields.filter(field => !headers.includes(field));
+          if (missingFields.length > 0) {
+            reject(`Erro: O CSV está faltando os seguintes campos: ${missingFields.join(", ")}`);
+          } else {
+            headersValidated = true;
+          }
+        })
         .on("data", (row) => {
+
+          if (!headersValidated) return;
+
+          if (!row.year || !row.title || !row.studios || !row.producers) {
+            logger.msg("warn", ["CSV"], `Linha inválida ignorada: ${JSON.stringify(row)}`);
+            return;
+          }
+
+          hasData = true;
+
           const movie = {};
           movie.year = parseInt(row.year);
           movie.title = row.title;
@@ -36,6 +57,12 @@ async function loadCSV() {
         })
         .on("end", async () => {
           try {
+
+            if (!hasData) {
+              reject(`Erro: O arquivo CSV não contém dados válidos.`);
+              return;
+            }
+
             for (const movieData of movies) {
               const { producers, ...movieInfo } = movieData;
               
@@ -55,7 +82,7 @@ async function loadCSV() {
                 });
               }
             }
-            logger.msg('debug', ['Express'], `Arquivo carregado com sucesso.`);
+            logger.msg('info', ['CSV'], `Arquivo carregado com sucesso.`);
             resolve();
           } catch (error) {
             reject(`Erro ao salvar os filmes no banco: ${error}`);
